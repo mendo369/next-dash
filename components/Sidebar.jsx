3ea419e6f138241aff1fa780248d01163c909842
Luis Mendoza <luis23luis33@gmail.com>
2023-02-28 17:21:58 -0500

    se modifica los iconos y se agragan las vistas para auth

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { RxSketchLogo, RxDashboard, RxPerson } from 'react-icons/rx'
-import { HiOutlineShoppingBag } from 'react-icons/hi'
-import { FiSettings } from 'react-icons/fi'
+import Dashboard from '../assets/icons/dashboard.svg'
+import User from '../assets/icons/user.svg'
+import Rifa from '../assets/icons/rifa.svg'
+import Money from '../assets/icons/money.svg'
 
 function Sidebar({ children }) {
     return (
@@ -12,28 +13,28 @@ function Sidebar({ children }) {
                 <div className="flex flex-col items-center">
                     <Link href={'/'}>
                         <div className='bg-purple-800 text-white p-3 rounded-lg inline-block'>
-                            <RxSketchLogo size={20} />
+                            <Image src={Money} />
                         </div>
                     </Link>
                     <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
                     <Link href={'/'}>
                         <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-                            <RxDashboard size={20} />
+                            <Image src={Rifa} />
                         </div>
                     </Link>
                     <Link href={'/customers'}>
                         <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-                            <RxPerson size={20} />
+                            <Image src={User} />
                         </div>
                     </Link>
                     <Link href={'/orders'}>
                         <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-                            <HiOutlineShoppingBag size={20} />
+                            <Image src={Dashboard} />
                         </div>
                     </Link>
                     <Link href={'/auth/login'}>
                         <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-                            <FiSettings size={20} />
+                            <Image src={Dashboard} />
                         </div>
                     </Link>
                 </div>
